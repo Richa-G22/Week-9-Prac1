a2b2a104545d270d7190f55bcaae3ba1c9a1d768
Billy <billy310682@gmail.com>
2023-06-02 00:35:57 -0400

    second-part

--- practice-for-week-05-conversion-functions-ascii-advanced-main/conversion-functions-ascii-advanced.js
+++ practice-for-week-05-conversion-functions-ascii-advanced-main/conversion-functions-ascii-advanced.js
@@ -9,32 +9,53 @@ Consult documentation on how to utilize the following methods:
 
 /* Base 2 to base 16 */
 function binaryToHexadecimal(blob) {
-  // Your code here
+const baseTen = parseInt(blob.slice(2), 2);
+//console.log(baseTen);
+return `0x${baseTen.toString(16)}`;
+
+// const numOne = 32;
+// const hex = numOne.toString(16);
+// console.log(`0x${hex}`);
 }
 
 /* Base 16 to base 2 */
 function hexadecimalToBinary(blob) {
-  // Your code here
+  //const baseTen = parseInt(blob);
+  const baseTen = parseInt(blob.slice(2), 16);
+  return `0b${baseTen.toString(2)}`;
 }
 
 /* Base 10 to ASCII */
 function decimalToAscii(blob) {
-  // Your code here
+  //console.log(blob);
+  return String.fromCharCode(blob);
 }
 
 /* Base 2 to ASCII */
 function binaryToAscii(blob) {
-  // Your code here
+  //const baseTen = parseInt(blob.slice(2), 2);
+  //return decimalToAscii(baseTen);
+  //return String.fromCharCode(baseTen);
+  return String.fromCharCode(blob);
 }
 
 /* Base 16 to ASCII */
 function hexadecimalToAscii(blob) {
-  // Your code here
+  //const baseTen = parseInt(blob);
+  //const baseTen = parseInt(blob.slice(2), 16);
+  return String.fromCharCode(blob);
 }
 
 /* ASCII to base 10 */
 function asciiToDecimal(blob) {
-  // Your code here
+  console.log(blob);
+  const arr = [];
+  for (let i = 0; i < blob.length; i++) {
+    arr.push(blob.charCodeAt(i));
+  }
+  
+  // const arr = blob.split(""); // ["T", "h", "e", "r", "e"]
+  // return arr.map((letter) => letter.charCodeAt(0));
 }
 
 // console.log('Binary to hexadecimal:')
